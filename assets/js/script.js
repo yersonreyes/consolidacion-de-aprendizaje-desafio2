15dc5bd8fa880a12c2e1d084cc19fe93a8398c33
Yerson Reyes <yerson.programador@gmail.com>
2022-04-16 23:35:38 -0400

    proyecto terminado

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,16 +5,20 @@ const $repoPagina = document.getElementById("repoPagina");
 const $resultados = document.getElementById("resultados");
 
 async function request(usuario, pagina = false, cantidad_repos = false) {
-  if (pagina == false || cantidad_repos == false) {
-    let response = await fetch(` https://api.github.com/users/${usuario}`);
-    let data = await response.json();
-    return data;
-  } else {
-    let response = await fetch(
-      ` https://api.github.com/users/${usuario}/repos?page=${pagina}&per_page=${cantidad_repos}.`
-    );
-    let data = await response.json();
-    return data;
+  try {
+    if (pagina == false || cantidad_repos == false) {
+      let response = await fetch(` https://api.github.com/users/${usuario}`);
+      let data = await response.json();
+      return data;
+    } else {
+      let response = await fetch(
+        ` https://api.github.com/users/${usuario}/repos?page=${pagina}&per_page=${cantidad_repos}.`
+      );
+      let data = await response.json();
+      return data;
+    }
+  } catch (error) {
+    console.log(error);
   }
 }
 
@@ -39,4 +43,29 @@ $button.addEventListener("click", async function () {
     paginaUsuario,
     repoPaginaUsuario
   );
+
+  $resultados.innerHTML = `
+  <div class="row">
+  <div class="col-6">
+    <h3>Datos de usuario</h3>
+    <img class="img-fluid" src=${dataUsuario.avatar_url} alt="">
+    <p>Nombre de usuario: ${dataUsuario.name}</p>
+    <p>Nombre de Login: ${dataUsuario.login}</p>
+    <p>Cantidad de Repositorios: ${dataUsuario.public_repos}</p>
+    <p>Localidad: ${dataUsuario.location}</p>
+    <p>Tipo de usuario: ${dataUsuario.type}</p>
+  </div>
+  <div class="col-6">
+  <h3>Nombre de repositorios</h3>
+  ${
+    dataRepo[0]
+      ? dataRepo.slice(0, 10).map((repo) => {
+          return `<p><a href=${repo.url}>${repo.name}</a></p>`;
+        })
+      : ""
+  }
+  </div>
+</div>
+
+  `;
 });
